966c2704d6d00754a0aac128fbbdbca6b245757e
Svein Olav Mork <sveinomork@gmail.com>
2025-01-26 18:47:45 +0100

    corrected spelling

--- generate_bat.py
+++ generate_bat.py
@@ -8,10 +8,10 @@ def gen_bat_file1(task_number, timeout, bat_name):
         # echo Task number 1 completed after 1 minute.
         bat_file.write(f'echo Task number {task_number} completed after {timeout} seconds.\n')
 
-def gen_bat_file2(python_file:Path,bat_file:Path,meomory:int=1024,processor:int=1):
+def gen_bat_file2(python_file:Path,bat_file:Path,memory:int=1024,processor:int=1,sleep:int=60):
     """Generate a bat file with a given task name."""
     with open(bat_file, "w") as bat:
         bat.write(f'echo {bat_file.name}\n')
         # timeout /t 60 >nul
-        bat.write(f'python {python_file}\n')
+        bat.write(f'python {python_file} {memory} {processor} {sleep} \n')
        
